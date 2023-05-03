c283c4a4e143aa1474730ac8f54edda53e70392f
dendelion666 <131227901+dendelion666@users.noreply.github.com>
2023-05-03 11:56:09 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    if (Math.randomBoolean()) {
+        basic.showIcon(IconNames.Skull)
+    } else {
+        basic.showIcon(IconNames.Square)
+    }
+})
